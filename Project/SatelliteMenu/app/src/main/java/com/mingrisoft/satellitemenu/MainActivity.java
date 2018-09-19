6d89c39cb746039e9055b31f6c309fe323388a2b
xiaoaxiaoxiaocao <547147242@qq.com>
2018-09-19 09:41:36 +0800

    RotateMenu

---
+++ Project/SatelliteMenu/app/src/main/java/com/mingrisoft/satellitemenu/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mingrisoft.satellitemenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import view.SatelliteMenu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
